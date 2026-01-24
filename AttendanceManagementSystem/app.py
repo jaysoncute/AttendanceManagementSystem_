ee54ffa7b7781fd63604c8a3a70a175cca1dd5fc
JaysonDionson <158709520+jaysoncute@users.noreply.github.com>
2026-01-24 18:47:18 +0800

    Update app.py

--- AttendanceManagementSystem/app.py
+++ AttendanceManagementSystem/app.py
@@ -1,1 +1,12 @@
+from flask import Flask
+from controllers.attendanceController import attendance_bp
+from models.attendanceModel import create_table
+
+app = Flask(__name__)
+app.register_blueprint(attendance_bp)
+
+create_table()  # Ensure table exists
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
