1d0c71f8e60099decf1590f64c8e3d9e1d92bdb7
JaysonDionson <158709520+jaysoncute@users.noreply.github.com>
2026-01-24 18:35:00 +0800

    Update attendanceService.py

--- AttendanceManagementSystem/services/attendanceService.py
+++ AttendanceManagementSystem/services/attendanceService.py
@@ -1,1 +1,12 @@
+from models import attendanceModel as model
+from datetime import datetime
+
+def mark_attendance(name):
+    date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    status = "Present"
+    model.add_attendance(name, date, status)
+    return f"Attendance marked for {name} at {date}"
+
+def list_attendance():
+    return model.get_attendance()
 
